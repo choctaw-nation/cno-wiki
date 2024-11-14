bd8c86a361534335a0af3eed62dade18334ce80e
KJ Roelke <kroelke@choctawnation.com>
2024-11-14 16:38:47 -0600

    update search form help text

--- wp-content/themes/cno-starter-theme/searchform.php
+++ wp-content/themes/cno-starter-theme/searchform.php
@@ -20,26 +20,15 @@ $search_button_classes = array(
 )
 ?>
 <div class="flex-grow-1" id="site-search">
-	<button type="button" aria-label="Search (Command+K)" class="<?php echo implode( ' ', $search_button_classes ); ?>" data-bs-toggle="modal" data-bs-target="#site-search-modal">
+	<button type="button" aria-label="Search (Keyboard Forward Slash)" class="<?php echo implode( ' ', $search_button_classes ); ?>" data-bs-toggle="modal"
+			data-bs-target="#site-search-modal">
 		<span class="d-flex align-items-center">
 			<svg width="20" height="20" viewBox="0 0 20 20" aria-hidden="true" class="opacity-75 text-light">
 				<path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
 						stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
 				</path>
 			</svg>
-			<span class="ps-3 pe-4 opacity-75 text-white">Search</span>
-		</span>
-		<span class="py-1 px-2 rounded-1 bg-black bg-opacity-50 opacity-75 d-flex justify-content-center align-items-center">
-			<?php
-			$kbd_button_classes = array(
-				'bg-transparent',
-				'text-white',
-				// 'opacity-75',
-				'font-gill-sans',
-			);
-			?>
-			<kbd class="<?php echo implode( ' ', $kbd_button_classes ); ?>">⌘</kbd>
-			<kbd class="<?php echo implode( ' ', $kbd_button_classes ); ?>">K</kbd>
+			<span class="ps-3 pe-4 opacity-75 text-white">Type <kbd>/</kbd> to search</span>
 		</span>
 	</button>
 </div>
