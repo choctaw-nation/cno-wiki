ae659f9a96c52051905c1bbefbb223a0b99b6645
KJ Roelke <kroelke@choctawnation.com>
2024-11-19 14:03:22 -0600

    update single and add headeroffset class

--- wp-content/themes/cno-starter-theme/single.php
+++ wp-content/themes/cno-starter-theme/single.php
@@ -7,22 +7,24 @@
 
 get_header();
 ?>
-<div class="container my-5">
-	<?php get_template_part( 'template-parts/single/nav', 'breadcrumbs' ); ?>
-</div>
-<article <?php post_class( 'container-fluid my-5' ); ?>>
-	<?php if ( has_post_thumbnail() ) : ?>
-	<div class="row">
-		<div class="col">
-			<?php the_post_thumbnail( 'full' ); ?>
+<main class="docs-main py-3">
+	<article <?php post_class( 'container-xxl my-5 docs-content' ); ?>>
+		<?php get_template_part( 'template-parts/single/nav', 'breadcrumbs' ); ?>
+		<?php if ( has_post_thumbnail() ) : ?>
+		<div class="row">
+			<div class="col">
+				<?php the_post_thumbnail( 'full' ); ?>
+			</div>
 		</div>
-	</div>
-	<?php endif; ?>
+		<?php endif; ?>
+		<?php
+		the_title( '<h1>', '</h1>' );
+		get_template_part( 'template-parts/content', 'post-meta', array( 'classes' => 'justify-content-start gap-3' ) );
+		the_content();
+		?>
+	</article>
 	<?php
-	the_title( '<h1>', '</h1>' );
-	get_template_part( 'template-parts/content', 'post-meta', array( 'classes' => 'justify-content-start gap-3' ) );
-	the_content();
+		get_template_part( 'template-parts/aside', 'table-of-contents' );
+		get_footer();
 	?>
-</article>
-<?php
-get_footer();
+</main>
--- wp-content/themes/cno-starter-theme/src/index.js
+++ wp-content/themes/cno-starter-theme/src/index.js
@@ -1,4 +1,6 @@
 import './styles/main.scss';
 import SiteSearch from './js/site-search/controller';
+import HeaderOffsetHandler from './js/CalcHeaderOffset';
 
 new SiteSearch();
+new HeaderOffsetHandler();
