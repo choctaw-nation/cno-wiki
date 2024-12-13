7d02a615d4ddee0dd862e65848ca6e82c3249668
KJ Roelke <kroelke@choctawnation.com>
2024-12-13 10:33:26 -0600

    add sidebar to single

--- wp-content/themes/cno-starter-theme/single.php
+++ wp-content/themes/cno-starter-theme/single.php
@@ -11,24 +11,27 @@ use ChoctawNation\Enqueue_Type;
 new Asset_Loader( 'toc-scroll', Enqueue_Type::script, 'modules' );
 get_header();
 ?>
-<main class="docs-main py-3">
-	<div <?php post_class( 'container-xxl docs-content' ); ?>>
-		<?php get_template_part( 'template-parts/single/nav', 'breadcrumbs' ); ?>
-		<?php if ( has_post_thumbnail() ) : ?>
-		<div class="row">
-			<div class="col">
-				<?php the_post_thumbnail( 'full' ); ?>
+<div class="container-xxl docs-container d-lg-grid gap-3">
+	<?php get_template_part( 'template-parts/aside', 'sidebar' ); ?>
+	<main class="docs-main py-3">
+		<div <?php post_class( 'container-xxl docs-content' ); ?>>
+			<?php get_template_part( 'template-parts/single/nav', 'breadcrumbs' ); ?>
+			<?php if ( has_post_thumbnail() ) : ?>
+			<div class="row">
+				<div class="col">
+					<?php the_post_thumbnail( 'full' ); ?>
+				</div>
 			</div>
+			<?php endif; ?>
+			<?php
+			the_title( '<h1>', '</h1>' );
+			get_template_part( 'template-parts/content', 'post-meta', array( 'classes' => 'justify-content-start gap-3' ) );
+			the_content();
+			?>
 		</div>
-		<?php endif; ?>
 		<?php
-		the_title( '<h1>', '</h1>' );
-		get_template_part( 'template-parts/content', 'post-meta', array( 'classes' => 'justify-content-start gap-3' ) );
-		the_content();
-		?>
-	</div>
-	<?php
 		get_template_part( 'template-parts/aside', 'table-of-contents' );
 		get_footer();
-	?>
-</main>
+		?>
+	</main>
+</div>
