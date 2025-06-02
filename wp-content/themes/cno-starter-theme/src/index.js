8a0940ad2f8f7069a112eae3fcfa6429a594ff46
KJ Roelke <kroelke@choctawnation.com>
2025-06-02 12:33:23 -0500

    Update changelog for version 1.1.0 and add BugReportFormHandler class for auto-populating contact form fields

--- wp-content/themes/cno-starter-theme/src/index.js
+++ wp-content/themes/cno-starter-theme/src/index.js
@@ -1,6 +1,8 @@
 import './styles/main.scss';
 import SiteSearch from './js/site-search/controller';
 import HeaderOffsetHandler from './js/CalcHeaderOffset';
+import BugReportFormHandler from './js/BugReportFormHandler';
 
 new SiteSearch();
 new HeaderOffsetHandler();
+new BugReportFormHandler();
